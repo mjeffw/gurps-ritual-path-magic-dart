07fe4ba59940036f55a44eab75949887e8726d77
mjeffw <mjeffw@bellsouth.net>
2018-08-06 08:16:54 -0400

    Added result_type

---
+++ lib/src/result_type.dart
@@ -0,0 +1,19 @@
+class ResultType {
+  static ResultType criticalFailure = ResultType('Critical Failure');
+  static ResultType failure = ResultType('Failure');
+  static ResultType success = ResultType('Success');
+  static ResultType criticalSuccess = ResultType('Critical Success');
+
+  static Map<String, ResultType> _values = {
+    criticalFailure.name: criticalFailure,
+    failure.name: failure,
+    success.name: success,
+    criticalSuccess.name: criticalSuccess,
+  };
+
+  final String name;
+
+  ResultType(this.name);
+
+  static ResultType fromString(String name) => _values[name];
+}
---
+++ test/result_type.dart
@@ -0,0 +1,52 @@
+import 'package:test/test.dart';
+import '../lib/src/result_type.dart';
+
+void main() {
+  group('Critical Failure', () {
+    ResultType result = ResultType.criticalFailure;
+
+    test('has name', () {
+      expect(result.name, equals('Critical Failure'));
+    });
+
+    test('can be constructed by name', () {
+      expect(result, equals(ResultType.fromString('Critical Failure')));
+    });
+  });
+
+  group('Failure', () {
+    ResultType result = ResultType.failure;
+
+    test('has name', () {
+      expect(result.name, equals('Failure'));
+    });
+
+    test('can be constructed by name', () {
+      expect(result, equals(ResultType.fromString('Failure')));
+    });
+  });
+
+  group('Success', () {
+    ResultType result = ResultType.success;
+
+    test('has name', () {
+      expect(result.name, equals('Success'));
+    });
+
+    test('can be constructed by name', () {
+      expect(result, equals(ResultType.fromString('Success')));
+    });
+  });
+
+  group('Critical Success', () {
+    ResultType result = ResultType.criticalSuccess;
+
+    test('has name', () {
+      expect(result.name, equals('Critical Success'));
+    });
+
+    test('can be constructed by name', () {
+      expect(result, equals(ResultType.fromString('Critical Success')));
+    });
+  });
+}
